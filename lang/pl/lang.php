3acd4d6ae128cf3921c914cb58a2e4a62b38a627
Andreas Haerter <development@andreas-haerter.com>
2011-03-17 10:38:33 +0100

    added Polish translation

---
+++ lang/pl/lang.php
@@ -0,0 +1,92 @@
+<?php
+
+/**
+ * Polish language for the "monobook" DokuWiki template
+ *
+ * If your language is not/only partially translated or you found an error/typo,
+ * have a look at the following files:
+ * - "/lib/tpl/monobook/lang/<your lang>/lang.php"
+ * - "/lib/tpl/monobook/lang/<your lang>/settings.php"
+ * If they are not existing, copy and translate the English ones. And don't
+ * forget to mail the translation to me,
+ * Andreas Haerter <development@andreas-haerter.com>. Thanks :-D.
+ *
+ *
+ * LICENSE: This file is open source software (OSS) and may be copied under
+ *          certain conditions. See COPYING file for details or try to contact
+ *          the author(s) of this file in doubt.
+ *
+ * @license GPLv2 (http://www.gnu.org/licenses/gpl2.html)
+ * @author Andrzej Pruszyński
+ * @link http://andreas-haerter.com/projects/dokuwiki-template-monobook
+ * @link http://www.dokuwiki.org/template:monobook
+ * @link http://www.dokuwiki.org/config:lang
+ * @link http://www.dokuwiki.org/devel:configuration
+ */
+
+
+//check if we are running within the DokuWiki environment
+if (!defined("DOKU_INC")){
+    die();
+}
+
+//tabs
+$lang["monobook_tab_article"] = "artykuł";
+$lang["monobook_tab_discussion"] = "dyskusja";
+$lang["monobook_tab_userpage"] = "strona użytkownika";
+$lang["monobook_tab_specialpage"] = "strony specjalne";
+$lang["monobook_tab_mytalk"] = "moja dyskusja";
+$lang["monobook_tab_exportodt"] = "eksport: ODT";
+$lang["monobook_tab_exportpdf"] = "eksport: PDF";
+
+//headlines for the different bars
+$lang["monobook_bar_views"] = "Views";
+$lang["monobook_bar_personnaltools"] = "narzędzia użytkownika";
+$lang["monobook_bar_navigation"] = "nawigacja";
+$lang["monobook_bar_toolbox"] = "narzędzia";
+$lang["monobook_bar_inotherlanguages"] = "w innych językach";
+$lang["monobook_bar_search"] = "wyszukiwanie";
+
+//buttons
+$lang["monobook_btn_go"] = "idź do";
+$lang["monobook_btn_search"] = "szukaj";
+
+//default toolbox
+$lang["monobook_toolbxdef_whatlinkshere"] = "wykaz odnośników";
+$lang["monobook_toolbxdef_upload"] = "przesyłanie plików";
+$lang["monobook_toolbxdef_siteindex"] = "indeks witryny";
+$lang["monobook_toolbxdef_print"] = "wersja do druku";
+$lang["monobook_toolboxdef_permanent"] = "link z wersją";
+$lang["monobook_toolboxdef_cite"] = "cytowanie";
+
+//cite this article
+$lang["monobook_cite_bibdetailsfor"] = "Szczegóły bibliograficzne strony";
+$lang["monobook_cite_pagename"] = "Nazwa strony";
+$lang["monobook_cite_author"] = "Autor";
+$lang["monobook_cite_publisher"] = "Wydawca";
+$lang["monobook_cite_dateofrev"] = "Data publikacji tej wersji";
+$lang["monobook_cite_dateretrieved"] = "Data dostępu";
+$lang["monobook_cite_permurl"] = "Adres URL tej wersji";
+$lang["monobook_cite_pageversionid"] = "Identyfikator wersji strony";
+$lang["monobook_cite_citationstyles"] = "Style cytowania strony";
+$lang["monobook_cite_checkstandards"] = "Please remember to check your manual of style, standards guide or instructor's guidelines for the exact syntax to suit your needs";
+$lang["monobook_cite_latexusepackagehint"] = "When using the LaTeX package url (\usepackage{url} somewhere in the preamble), which tends to give much more nicely formatted web addresses, the following may be preferred";
+$lang["monobook_cite_retrieved"] = "Retrieved";
+$lang["monobook_cite_from"] = "from";
+$lang["monobook_cite_in"] = "In";
+$lang["monobook_cite_accessed"] = "Accessed";
+$lang["monobook_cite_cited"] = "Cited";
+$lang["monobook_cite_lastvisited"] = "Last visited";
+$lang["monobook_cite_availableat"] = "Available at";
+$lang["monobook_cite_discussionpages"] = "DokuWiki talk pages";
+$lang["monobook_cite_markup"] = "Markup";
+$lang["monobook_cite_result"] = "Result";
+$lang["monobook_cite_thisversion"] = "this version";
+
+//other
+$lang["monobook_accessdenied"] = "Dostęp zabroniony";
+$lang["monobook_fillplaceholder"] = "Please fill this placeholder";
+$lang["monobook_donate"] = "Donate";
+$lang["monobook_mdtemplatefordw"] = "monobook template for DokuWiki";
+$lang["monobook_recentchanges"] = "ostatnie zmiany";
+
