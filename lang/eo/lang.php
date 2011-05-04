af166a94991668612db0fbc673c594c34a628381
Andreas Haerter <development@andreas-haerter.com>
2011-05-04 20:27:55 +0200

    added Esperanto translation

---
+++ lang/eo/lang.php
@@ -0,0 +1,69 @@
+<?php
+
+/**
+ * Esperanto language for the "monobook" DokuWiki template
+ *
+ * If your language is not/only partially translated or you found an error/typo,
+ * have a look at the following files:
+ * - "/lib/tpl/monobook/lang/<your lang>/lang.php"
+ * - "/lib/tpl/monobook/lang/<your lang>/settings.php"
+ * If they are not existing, copy and translate the English ones. And don't
+ * forget to mail the translation to me,
+ * Andreas Haerter <development@andreas-haerter.com>. Thanks :-D.
+ *
+ *
+ * LICENSE: This file is open source software (OSS) and may be copied under
+ *          certain conditions. See COPYING file for details or try to contact
+ *          the author(s) of this file in doubt.
+ *
+ * @license GPLv2 (http://www.gnu.org/licenses/gpl2.html)
+ * @author Aleksej Kuznecov <ka2pink@gmail.com>
+ * @link http://andreas-haerter.com/projects/dokuwiki-template-monobook
+ * @link http://www.dokuwiki.org/template:monobook
+ * @link http://www.dokuwiki.org/config:lang
+ * @link http://www.dokuwiki.org/devel:configuration
+ */
+
+//Language constant, for the monobook DokuWiki template  :  
+
+//check if we are running within the DokuWiki environment
+if (!defined("DOKU_INC")){
+    die();
+}
+
+//tabs
+$lang["monobook_tab_article"] = "Artikolo";
+$lang["monobook_tab_discussion"] = "Diskutejo";
+$lang["monobook_tab_userpage"] = "Uzantopaĝo";
+$lang["monobook_tab_specialpage"] = "Specialaj paĝoj";
+$lang["monobook_tab_mytalk"] = "Mia diskutejo";
+$lang["monobook_tab_exportodt"] = "Eksport: ODT";
+$lang["monobook_tab_exportpdf"] = "Eksport: PDF";
+
+//Constants for the search toolbar
+$lang['monobook_bar_views'] = 'Views';
+$lang['monobook_bar_personnaltools'] = 'Personalaj iloj';
+$lang['monobook_bar_navigation'] = 'Navigado';
+$lang['monobook_bar_toolbox'] = 'Ilaro';
+$lang['monobook_bar_inotherlanguages'] = 'En aliaj lingvoj';
+$lang['monobook_bar_search'] = 'Serĉo';
+
+//buttons
+$lang['monobook_btn_go'] = 'Ek!';
+$lang['monobook_btn_search'] = 'Serĉo';
+
+//default toolbox
+$lang["monobook_toolbxdef_whatlinkshere"] = "Ligoj tien";
+$lang["monobook_toolbxdef_upload"] = "Ŝarĝi dosieron";
+$lang["monobook_toolbxdef_siteindex"] = "Indekso";
+$lang["monobook_toolbxdef_print"] = "Printebla versio";
+$lang["monobook_toolboxdef_permanent"] = "Rekta ligo";
+$lang["monobook_toolboxdef_cite"] = "Citi tiun artikolon";
+
+//other
+$lang["monobook_accessdenied"] = "Aliro malpermesita";
+$lang["monobook_fillplaceholder"] = "Bonvolu plenigi tiun kampon";
+$lang["monobook_donate"] = "Donaci";
+$lang["monobook_mdtemplatefordw"] = "ŝablono monobook por DokuWiki";
+$lang["monobook_recentchanges"] = "Lastaj ŝanĝoj";
+
