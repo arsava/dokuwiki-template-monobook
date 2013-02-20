eba04ed042a965e60353a589e8092106b458d19e
Andreas Haerter <ah@syn-systems.com>
2013-02-20 14:16:15 +0100

    Add Chinese (as used in Taiwan) language files

---
+++ lang/zh-tw/lang.php
@@ -0,0 +1,98 @@
+<?php
+
+/**
+ * Chinese (as used in Taiwan) language for the "monobook" DokuWiki template
+ *
+ * If your language is not/only partially translated or you found an error/typo,
+ * have a look at the following files:
+ * - "/lib/tpl/monobook/lang/<your lang>/lang.php"
+ * - "/lib/tpl/monobook/lang/<your lang>/settings.php"
+ * If they are not existing, copy and translate the English ones. And don't
+ * forget to mail the translation to us,
+ * SYN Systems <dokuwiki@dev.syn-systems.com>. Thanks :-D.
+ *
+ *
+ * LICENSE: This file is open source software (OSS) and may be copied under
+ *          certain conditions. See COPYING file for details or try to contact
+ *          the author(s) of this file in doubt.
+ *
+ * @license GPLv2 (http://www.gnu.org/licenses/gpl2.html)
+ * @author 呂理群
+ * @link http://www.dokuwiki.org/template:monobook
+ * @link http://www.dokuwiki.org/config:lang
+ * @link http://www.dokuwiki.org/devel:configuration
+ */
+
+
+//check if we are running within the DokuWiki environment
+if (!defined("DOKU_INC")){
+    die();
+}
+
+//tabs
+$lang["monobook_tab_article"] = "文章";
+$lang["monobook_tab_discussion"] = "討論";
+$lang["monobook_tab_userpage"] = "使用者頁面";
+$lang["monobook_tab_specialpage"] = "特定頁面";
+$lang["monobook_tab_mytalk"] = "我的對話頁";
+$lang["monobook_tab_exportodt"] = "匯出成: ODT檔";
+$lang["monobook_tab_exportpdf"] = "匯出成: PDF檔";
+
+//headlines for the different bars
+$lang["monobook_bar_views"] = "檢視";
+$lang["monobook_bar_personnaltools"] = "個人工具";
+$lang["monobook_bar_navigation"] = "導覽";
+$lang["monobook_bar_toolbox"] = "工具箱";
+$lang["monobook_qrcodebox"] = "QR Code";
+$lang["monobook_bar_inotherlanguages"] = "其他語言";
+$lang["monobook_bar_search"] = "搜尋";
+
+//buttons
+$lang["monobook_btn_go"] = "轉到指定頁";
+$lang["monobook_btn_search"] = "搜尋";
+
+//default toolbox
+$lang["monobook_toolbxdef_whatlinkshere"] = "相關連接";
+$lang["monobook_toolbxdef_upload"] = "上傳檔案";
+$lang["monobook_toolbxdef_siteindex"] = "網站地圖";
+$lang["monobook_toolbxdef_print"] = "預覽列印";
+$lang["monobook_toolboxdef_permanent"] = "永久鏈接";
+$lang["monobook_toolboxdef_cite"] = "引用這篇文章";
+
+//qr code box
+$lang["monobook_qrcodebox_qrcode"] = "QR Code";
+$lang["monobook_qrcodebox_genforcurrentpage"] = "對當前頁面產生";
+$lang["monobook_qrcodebox_urlofcurrentpage"] = "當前頁的 QR Code (行動裝置易存取)";
+
+//cite this article
+$lang["monobook_cite_bibdetailsfor"] = "詳細書目";
+$lang["monobook_cite_pagename"] = "頁面名稱";
+$lang["monobook_cite_author"] = "作者";
+$lang["monobook_cite_publisher"] = "發佈者";
+$lang["monobook_cite_dateofrev"] = "本次修訂的日期";
+$lang["monobook_cite_dateretrieved"] = "日期檢索";
+$lang["monobook_cite_permurl"] = "永久網址";
+$lang["monobook_cite_pageversionid"] = "頁面版本 ID";
+$lang["monobook_cite_citationstyles"] = "引用樣式";
+$lang["monobook_cite_checkstandards"] = "請記得確切的語法的風格，以檢查您的手冊、指南或其他的指導方針，以滿足您的需求";
+$lang["monobook_cite_latexusepackagehint"] = "When using the LaTeX package url (\usepackage{url} somewhere in the preamble), which tends to give much more nicely formatted web addresses, the following may be preferred";
+$lang["monobook_cite_retrieved"] = "檢索";
+$lang["monobook_cite_from"] = "from";
+$lang["monobook_cite_in"] = "In";
+$lang["monobook_cite_accessed"] = "存取";
+$lang["monobook_cite_cited"] = "Cited";
+$lang["monobook_cite_lastvisited"] = "最後一次拜訪";
+$lang["monobook_cite_availableat"] = "Available at";
+$lang["monobook_cite_discussionpages"] = "DokuWiki talk pages";
+$lang["monobook_cite_markup"] = "標記";
+$lang["monobook_cite_result"] = "結果";
+$lang["monobook_cite_thisversion"] = "這個版本";
+
+//other
+$lang["monobook_accessdenied"] = "拒絕存取";
+$lang["monobook_fillplaceholder"] = "請填寫或不啟用";
+$lang["monobook_donate"] = "捐助";
+$lang["monobook_mdtemplatefordw"] = "monobook template for DokuWiki";
+$lang["monobook_qrcodebtn"] = "QR Code 產生器";
+$lang["monobook_recentchanges"] = "最新更新";
+
---
+++ lang/zh-tw/settings.php
@@ -0,0 +1,81 @@
+<?php
+
+/**
+ * Chinese (as used in Taiwan) language for the Config Manager
+ *
+ * If your language is not/only partially translated or you found an error/typo,
+ * have a look at the following files:
+ * - "/lib/tpl/monobook/lang/<your lang>/lang.php"
+ * - "/lib/tpl/monobook/lang/<your lang>/settings.php"
+ * If they are not existing, copy and translate the English ones. And don't
+ * forget to mail the translation to us,
+ * SYN Systems <dokuwiki@dev.syn-systems.com>. Thanks :-D.
+ *
+ *
+ * LICENSE: This file is open source software (OSS) and may be copied under
+ *          certain conditions. See COPYING file for details or try to contact
+ *          the author(s) of this file in doubt.
+ *
+ * @license GPLv2 (http://www.gnu.org/licenses/gpl2.html)
+ * @author 呂理群
+ * @link http://www.dokuwiki.org/template:monobook
+ * @link http://www.dokuwiki.org/config:lang
+ * @link http://www.dokuwiki.org/devel:configuration
+ */
+
+
+//check if we are running within the DokuWiki environment
+if (!defined("DOKU_INC")){
+    die();
+}
+
+//user pages
+$lang["monobook_userpage"]    = "使用「使用者頁面」嗎?";
+$lang["monobook_userpage_ns"] = "如果要, 使用 ':namespace:' 為使用者頁面的 root:";
+
+//discussion pages
+$lang["monobook_discuss"]    = "使用「討論的標籤/網站」嗎?";
+$lang["monobook_discuss_ns"] = "如果要, 使用 ':namespace:' 為討論頁面的 root:";
+
+//site notice
+$lang["monobook_sitenotice"]           = "顯示「行條式的網站注意事項」嗎?";
+$lang["monobook_sitenotice_location"]  = "如果要, 使用以下頁面當成行條式網站注意事項:";
+$lang["monobook_sitenotice_translate"] = "If yes and <a href=\"http://www.dokuwiki.org/plugin:translation\">Translation plugin</a> available: load language specific site wide notice?<br />The wiki page of the translated site notice(s) is [value of 'monobook_sitenotice_location']_[iso lang code] (e.g. ':wiki:site_notice_de').";
+
+//navigation
+$lang["monobook_navigation"]           = "顯示「導覽」嗎?";
+$lang["monobook_navigation_location"]  = "如果要, 使用以下頁面當成導覽頁面:";
+$lang["monobook_navigation_translate"] = "If yes and <a href=\"http://www.dokuwiki.org/plugin:translation\">Translation plugin</a> available: load language specific navigation?<br />The wiki page of the translated navigation(s) is [value of 'monobook_navigation_location']_[iso lang code] (e.g. ':wiki:navigation_de').";
+
+//custom copyright notice
+$lang["monobook_copyright"]           = "顯示「版權注意事項」嗎?";
+$lang["monobook_copyright_default"]   = "如果要, 使用預設的「版權注意事項」嗎?";
+$lang["monobook_copyright_location"]  = "如果不使用預設值, 使用以下頁面當成「版權注意事項」:";
+$lang["monobook_copyright_translate"] = "If not default and <a href=\"http://www.dokuwiki.org/plugin:translation\">Translation plugin</a> available: load language specific copyright notice?<br />The wiki page of the translated copyright notice(s) is [value of 'monobook_copyright_location']_[iso lang code] (e.g. ':wiki:copyright_de').";
+
+//search form
+$lang["monobook_search"] = "顯示從哪搜尋嗎?";
+
+//toolbox
+$lang["monobook_toolbox"]               = "顯示「工具箱」嗎?";
+$lang["monobook_toolbox_default"]       = "如果要, 使用「預設工具箱」?";
+$lang["monobook_toolbox_default_print"] = "如果「預設工具箱」已使用中, 則顯示「可列印版本的連結」?";
+$lang["monobook_toolbox_location"]      = "如果不是預設值, 使用以下頁面當成「工具箱位置」:";
+
+//qr code box
+$lang["monobook_qrcodebox"] = "Show box with QR Code of current wiki page URL (for easy URL transfer to mobile browser)?";
+
+//donation link/button
+$lang["monobook_donate"]          = "顯示捐助連接/按紐?";
+$lang["monobook_donate_url"]      = "使用以下捐助的 URL:";
+
+//TOC
+$lang["monobook_toc_position"] = "Table of contents (TOC) position";
+
+//other stuff
+$lang["monobook_breadcrumbs_position"]  = "Position of breadcrumb navigation (if enabled):";
+$lang["monobook_youarehere_position"]   = "Position of 'You are here' navigation (if enabled):";
+$lang["monobook_cite_author"]           = "在引用文章中顯示作者姓名:";
+$lang["monobook_loaduserjs"]            = "載入 'monobook/user/user.js'?";
+$lang["monobook_closedwiki"]            = "已關閉頁面 (most links/tabs/boxes are hidden until user is logged in)?";
+
