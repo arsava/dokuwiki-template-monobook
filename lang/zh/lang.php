05434fb117c709875f512af618b9b556c8a1e74c
Andreas Haerter <ah@foundata.net>
2013-11-17 13:35:21 +0100

    Add Chinese (simplified) language files

---
+++ lang/zh/lang.php
@@ -0,0 +1,99 @@
+<?php
+
+/**
+ * Chinese (Simplified) language for the "monobook" DokuWiki template
+ *
+ * If your language is not/only partially translated or you found an error/typo,
+ * have a look at the following files:
+ * - "/lib/tpl/monobook/lang/<your lang>/lang.php"
+ * - "/lib/tpl/monobook/lang/<your lang>/settings.php"
+ * If they are not existing, copy and translate the English ones.
+ *
+ * Don't forget to mail your translation to
+ *   Andreas Haerter <development@andreas-haerter.com>
+ * Thanks :-D.
+ *
+ *
+ * LICENSE: This file is open source software (OSS) and may be copied under
+ *          certain conditions. See COPYING file for details or try to contact
+ *          the author(s) of this file in doubt.
+ *
+ * @license GPLv2 (http://www.gnu.org/licenses/gpl2.html)
+ * @author Rubyy
+ * @link http://www.dokuwiki.org/template:monobook
+ * @link http://www.dokuwiki.org/config:lang
+ * @link http://www.dokuwiki.org/devel:configuration
+ */
+
+
+//check if we are running within the DokuWiki environment
+if (!defined("DOKU_INC")){
+    die();
+}
+
+//tabs
+$lang["monobook_tab_article"] = "文章";
+$lang["monobook_tab_discussion"] = "讨论";
+$lang["monobook_tab_userpage"] = "使用者页面";
+$lang["monobook_tab_specialpage"] = "特定页面";
+$lang["monobook_tab_mytalk"] = "我的对话页";
+$lang["monobook_tab_exportodt"] = "导出为ODT文件";
+$lang["monobook_tab_exportpdf"] = "导出为PDF文件";
+
+//headlines for the different bars
+$lang["monobook_bar_views"] = "查看";
+$lang["monobook_bar_personnaltools"] = "个人工具";
+$lang["monobook_bar_navigation"] = "导航";
+$lang["monobook_bar_toolbox"] = "工具箱";
+$lang["monobook_qrcodebox"] = "二维码";
+$lang["monobook_bar_inotherlanguages​​"] = "其他语言";
+$lang["monobook_bar_search"] = "查找";
+
+//buttons
+$lang["monobook_btn_go"] = "转到指定页";
+$lang["monobook_btn_search"] = "查找";
+
+//default toolbox
+$lang["monobook_toolbxdef_whatlinkshere"] = "相关链接";
+$lang["monobook_toolbxdef_upload"] = "上传文件";
+$lang["monobook_toolbxdef_siteindex"] = "网站地图";
+$lang["monobook_toolbxdef_print"] = "预览打印";
+$lang["monobook_toolboxdef_permanent"] = "永久链接";
+$lang["monobook_toolboxdef_cite"] = "引用这篇文章";
+
+//qr code box
+$lang["monobook_qrcodebox_qrcode"] = "二维码";
+$lang["monobook_qrcodebox_genforcurrentpage"] = "生成当前页面的二维码";
+$lang["monobook_qrcodebox_urlofcurrentpage"] = "当前页的二维码(QR Code)";
+
+//cite this article
+$lang["monobook_cite_bibdetailsfor"] = "详细书目";
+$lang["monobook_cite_pagename"] = "页面名称";
+$lang["monobook_cite_author"] = "作者";
+$lang["monobook_cite_publisher"] = "发布者";
+$lang["monobook_cite_dateofrev"] = "本次修订的日期";
+$lang["monobook_cite_dateretrieved"] = "日期检索";
+$lang["monobook_cite_permurl"] = "永久网址";
+$lang["monobook_cite_pageversionid"] = "页面版本ID";
+$lang["monobook_cite_citationstyles"] = "引用样式";
+$lang["monobook_cite_checkstandards"] = "请记得确切的语法的风格，以检查您的手册、指南或其他的指导方针，以满足您的需求";
+$lang["monobook_cite_latexusepackagehint"] = "当使用往往能给予更好的Web地址格式的LaTeX包 (在开头的某个位置的\usepackage{url})的URL时, 这可能是首选";
+$lang["monobook_cite_retrieved"] = "搜索";
+$lang["monobook_cite_from"] = "来源";
+$lang["monobook_cite_in"] = "在";
+$lang["monobook_cite_accessed"] = "访问";
+$lang["monobook_cite_cited"] = "被引用";
+$lang["monobook_cite_lastvisited"] = "最后一次查看";
+$lang["monobook_cite_availableat"] = "可用时间";
+$lang["monobook_cite_discussionpages"] = "DokuWiki讨论页";
+$lang["monobook_cite_markup"] = "标记";
+$lang["monobook_cite_result"] = "结果";
+$lang["monobook_cite_thisversion"] = "这个版本";
+
+//other
+$lang["monobook_accessdenied"] = "拒绝访问";
+$lang["monobook_fillplaceholder"] = "请填写这里,否则请不要启用本功能.";
+$lang["monobook_donate"] = "捐助";
+$lang["monobook_mdtemplatefordw"] = "为Dokuwiki设计的Monobook模板";
+$lang["monobook_qrcodebtn"] = "二维码生成器";
+$lang["monobook_recentchanges"] = "最近更新";
---
+++ lang/zh/settings.php
@@ -0,0 +1,83 @@
+<?php
+
+/**
+ * Chinese (Simplified) language for the Config Manager
+ *
+ * If your language is not/only partially translated or you found an error/typo,
+ * have a look at the following files:
+ * - "/lib/tpl/monobook/lang/<your lang>/lang.php"
+ * - "/lib/tpl/monobook/lang/<your lang>/settings.php"
+ * If they are not existing, copy and translate the English ones.
+ *
+ * Don't forget to mail your translation to
+ *   Andreas Haerter <development@andreas-haerter.com>
+ * Thanks :-D.
+ *
+ *
+ * LICENSE: This file is open source software (OSS) and may be copied under
+ *          certain conditions. See COPYING file for details or try to contact
+ *          the author(s) of this file in doubt.
+ *
+ * @license GPLv2 (http://www.gnu.org/licenses/gpl2.html)
+ * @author Rubyy
+ * @link http://www.dokuwiki.org/template:monobook
+ * @link http://www.dokuwiki.org/config:lang
+ * @link http://www.dokuwiki.org/devel:configuration
+ */
+
+
+//check if we are running within the DokuWiki environment
+if (!defined("DOKU_INC")){
+    die();
+}
+
+//user pages
+$lang["monobook_userpage"]    = "开启用户页么？";
+$lang["monobook_userpage_ns"] = "如果开启，使用 ':namespace' 作为用户页的根页面";
+
+//discussion pages
+$lang["monobook_discuss"]    = "开启讨论页么？";
+$lang["monobook_discuss_ns"] = "如果开启，使用 ':namespace' 作为讨论页的根页面";
+
+//site notice
+$lang["monobook_sitenotice"]           = "显示'站点公告'么?";
+$lang["monobook_sitenotice_location"]  = "如果显示，使用 ':namespace' 作为站点公告页面";
+$lang["monobook_sitenotice_translate"] = "如果开启了翻译插件,站点公告的不同语言翻译在:(例如':wiki:site_notice_en').";
+
+//navigation
+$lang["monobook_navigation"]           = "显示'导航栏'么?";
+$lang["monobook_navigation_location"]  = "如果显示，使用 ':namespace' 作为导航栏页面";
+$lang["monobook_navigation_translate"] = "如果开启了翻译插件,导航栏的不同语言翻译在:(例如':wiki:site_navigation_en').";
+
+//custom copyright notice
+$lang["monobook_copyright"]           = "显示版权声明么?";
+$lang["monobook_copyright_default"]   = "如果显示，使用 ':namespace' 作为版权声明页面";
+$lang["monobook_copyright_location"]  = "如果不使用预设值, 使用以下页面作为版权声明页面";
+$lang["monobook_copyright_translate"] = "如果开启了翻译插件,版权声明的的不同语言翻译在:(例如':wiki:site_copyright_en').";
+
+//search form
+$lang["monobook_search"] = "显示查找栏么?";
+
+//toolbox
+$lang["monobook_toolbox"]               = "显示'工具箱'么?";
+$lang["monobook_toolbox_default"]       = "如果显示,使用预设工具箱页面么?";
+$lang["monobook_toolbox_default_print"] = "如果使用预设工具箱页面,要显示'可打印版本'的链接么?";
+$lang["monobook_toolbox_location"]      = "如果不适用预设, 使用以下页面作为工具箱页面:";
+
+//qr code box
+$lang["monobook_qrcodebox"] = "显示当前页面URL生成的二维码么(对于手机浏览更方便)?";
+
+//donation link/button
+$lang["monobook_donate"]          = "显示捐助按钮么?";
+$lang["monobook_donate_url"]      = "使用以下捐助URL:";
+
+//TOC
+$lang["monobook_toc_position"] = "内容表(TOC)的位置";
+
+//other stuff
+$lang["monobook_breadcrumbs_position"]  = "'我的足迹'导航的位置:";
+$lang["monobook_youarehere_position"]   = "'当前位置'导航的位置:";
+$lang["monobook_cite_author"]           = "在引用文章中显示作者姓名:";
+$lang["monobook_loaduserjs"]            = "是否载入'monobook/user/user.js'?";
+$lang["monobook_closedwiki"]            = "是否为封闭Wiki(大多数页面对于未登录的游客不可显示)?";
+
